c735b9d3e9a868243a95809c6b9e2a78ef11ea37
mehD <mehdikatibeh81@gmail.com>
2026-05-20 13:47:19 -0700

    UPDATE[app]: the main.py file now manages the pipeline completely.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from graphical_interface import draw_grid
 from map_builder import map_builder
 from transition import *
 from update_Q_table import update_Q_table
@@ -7,44 +8,61 @@ import random
 
 
 #configuration
-states = [4,4]
+states = [20,20]
 actions = [0,1,2,3] #0->up 1->right 2->down 3->left
-epsilon = 0.1
+epsilon = 0.5
 learning_rate = 0.1
 discount_factor = 0.1
+episodes = 50000
+max_steps = 100
 
 #map
 # road=0, joungle=1, swamp=2, mountain=3, desert=4
-rewards = [-1, -3, -8, -5, -6]
+rewards = [-1, -3, -8, -5, -6, 1000]
 start = [0,0]
-goal = [3,3]
+goal = [19,19]
+
 
 Q_table = np.zeros(((states[0]*states[1]),(len(actions))))
-map = map_builder(states)
-
-#agent
-current_state = start
-round = 0
-score = 0
-
-rows, cols = states
-while current_state != goal:
-    previous_state = current_state
-    action = select_action(current_state=current_state, epsilon=epsilon, Q_table=Q_table, rows=rows, cols=cols)
-    current_state, score, reward = apply_transition(current_state=current_state, action=action, map=map, rewards=rewards, score=score)
-    Q_table = update_Q_table(
-
-        Q_table=Q_table,
-        previous_state=previous_state,
-        current_state=current_state,
-        rows=rows,
-        cols=cols,
-        action=action,
-        reward=reward,
-        learning_rate=learning_rate,
-        discount_factor=discount_factor
-    )
-    round += 1
+grid_map = map_builder(states)
+grid_map[goal[0]][goal[1]] = 5
 
 
+for episode in range(episodes):
+    current_state = start
+    score = 0
+
+    for step in range(max_steps):
+
+        if current_state == goal:
+            break
+
+        previous_state = current_state
+        action = select_action(current_state=current_state, epsilon=epsilon, Q_table=Q_table, states=states)
+        current_state, score, reward = apply_transition(current_state=current_state, action=action, grid_map=grid_map, states=states, rewards=rewards, score=score)
+        Q_table = update_Q_table(
+
+            Q_table=Q_table,
+            previous_state=previous_state,
+            current_state=current_state,
+            states=states,
+            action=action,
+            reward=reward,
+            learning_rate=learning_rate,
+            discount_factor=discount_factor
+        )
+
+print(Q_table)
+path = get_best_path(Q_table=Q_table, grid_map=grid_map, states=states, rewards=rewards, start=start, goal=goal)
+print(path)
+draw_grid(grid_map, path, start, goal)
+
+
+
+#TODO fixing ping ponging.
+#TODO implementing epsilon decay.
+#TODO implementing random start.
+#TODO implementing walls.
+#TODO implementing traps.
+#TODO implementing config file
 
