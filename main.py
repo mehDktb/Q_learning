ad5e9bcb8d6091fb6d369205814e43f37d7eafb1
mehD <mehdikatibeh81@gmail.com>
2026-05-22 04:17:29 -0700

    CHORE[app]: add a random_state function to make the code cleaner.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from graphical_interface import draw_grid
 from map_builder import map_builder
 from transition import *
 from update_Q_table import update_Q_table
+from random_start import random_state
 import numpy as np
 import random
 
@@ -13,11 +14,12 @@ actions = [0,1,2,3] #0->up 1->right 2->down 3->left
 epsilon = 1.0
 epsilon_min = 0.05
 decay_rate = 0.995
-learning_rate = 0.4
+learning_rate = 0.3
 discount_factor = 0.9
 episodes = 50000
 max_steps = 200
 random_start = True
+
 
 #map
 # road=0, joungle=1, swamp=2, mountain=3, desert=4
@@ -37,9 +39,12 @@ grid_map[goal[0]][goal[1]] = 5
 
 
 for episode in range(episodes):
-    current_state = start
-    score = 0
+    episode_random_start_rate = max(0.1, 0.5 * (0.9995 ** episode))
+    current_state = random_state(start=start, random_rate=episode_random_start_rate, rows=rows, cols=cols)
+    print("start from : ", current_state)
+    epsilon = max(epsilon_min, epsilon * decay_rate)
     visited_tuple = set()
+    score = 0
 
     for step in range(max_steps):
 
@@ -75,8 +80,6 @@ draw_grid(grid_map, path, start, goal)
 
 
 
-#TODO implementing epsilon decay.
-#TODO implementing random start.
 #TODO implementing walls.
 #TODO implementing traps.
 #TODO implementing config file
---
+++ random_start.py
@@ -0,0 +1,9 @@
+import random
+
+def random_state(start ,random_rate, rows, cols):
+    if random.uniform(0, 1) < random_rate:
+        state = [random.randint(0, rows - 1), random.randint(0, cols - 1)]
+    else:
+        state = start.copy()
+
+    return state
